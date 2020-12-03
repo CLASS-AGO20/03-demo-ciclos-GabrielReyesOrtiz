01d2482d50725cb029bb1183794f8f478b7832a2
GabrielReyesOrtiz <greyes17@ucol.mx>
2020-12-03 17:31:28 -0600

    Optimized sumarParesFor

--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ export default class App {
   }
   contarImparesFor(inicio, fin) {
     let conteo = 0;
-    for (let i = inicio; i <= fin; i = i + 1) {
+    for (let i = inicio; i <= fin; i++) {
       if (i % 2 != 0) {
-        conteo = conteo + 1;
+        conteo++;
       }
     }
     return conteo;
